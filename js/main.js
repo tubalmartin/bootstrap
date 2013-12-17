d0b7114a7cfe54573c7e9ded345784c7d8577461
Túbal Martín <tubal.martin@hola-internet.com>
2013-12-17 12:17:22 +0100

    Added main.js to include all modules

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Require every Bootstrap JS module
+ */
+define([
+    './affix',
+    './alert',
+    './button',
+    './carousel',
+    './collapse',
+    './dropdown',
+    './modal',
+    './popover',
+    './scrollspy',
+    './tab',
+    './tooltip',
+    './transition'
+], function() {});
